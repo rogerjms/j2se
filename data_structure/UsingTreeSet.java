330118989303adcb23ae253068d5c96f30a989cb
xiaoke <nygxk88@163.com>
2015-02-24 12:30:24 +0800

    treeset
    treeset example

---
+++ data_structure/UsingTreeSet.java
@@ -0,0 +1,23 @@
+﻿// 演示TreeSet的用法。
+import java.util.*;
+public class UsingTreeSet {
+  public static void main(String args[]) {
+	  //创建哈希集
+	  Set set = new HashSet();
+	  String text = "You are good.You are very good.You are great." +
+		  "You are wonderful!";
+	  //从文本中析取单词
+	  StringTokenizer st = new  StringTokenizer(text," .!");
+	  while(st.hasMoreTokens())
+		  set.add(st.nextToken());
+
+	  TreeSet treeSet = new TreeSet(set);
+	  System.out.println(treeSet);
+	  //获取迭代器
+	  Iterator iterator = treeSet.iterator();
+	  //显示树集中的元素
+	  while (iterator.hasNext()) {
+		  System.out.print(iterator.next() + " ");
+	  }
+  }
+}
