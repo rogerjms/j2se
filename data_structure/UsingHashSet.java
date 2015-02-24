ad2ada9bb670d234ac27adbe773fb1b5706003f9
xiaoke <nygxk88@163.com>
2015-02-24 12:25:31 +0800

    hashset
    hashset example

---
+++ data_structure/UsingHashSet.java
@@ -0,0 +1,21 @@
+﻿//使用HashSet和Iterator。
+import java.util.*;
+public class UsingHashSet {
+  public static void main(String args[]) {
+	  //创建哈希集
+	  Set set = new HashSet();
+		  String text = "You are good.You are very good.You are great." +
+		  "You are wonderful!";
+	  //从文本中析取单词
+	  StringTokenizer st = new  StringTokenizer(text," .!");
+	  while(st.hasMoreTokens())
+		  set.add(st.nextToken());
+	  System.out.println(set);
+	  //获取迭代器
+	  Iterator iterator = set.iterator();
+	  //显示哈希集中的元素
+	  while (iterator.hasNext()) {
+		  System.out.print(iterator.next() + " ");
+	  }
+  }
+}
