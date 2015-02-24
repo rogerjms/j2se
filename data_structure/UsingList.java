fc83475a6ea579223c191e32f17c2c6c732c53ab
xiaoke <nygxk88@163.com>
2015-02-24 12:28:20 +0800

    list
    list examples

---
+++ data_structure/UsingList.java
@@ -0,0 +1,36 @@
+﻿//演示ArrayList类和LinkedList类的用法。
+import java.util.*;
+public class UsingList {
+  public static void main(String args[]) {
+	  ArrayList arrayList = new  ArrayList();
+	  arrayList.add(new Integer(5));
+	  arrayList.add(new Integer(8));
+	  arrayList.add(new Integer(6));
+	  arrayList.add(new Integer(30));
+	  arrayList.add(new Integer(4));
+	  arrayList.add(0,new Integer(40));
+	  arrayList.add(2,new Integer(50));
+
+	  System.out.println("A list of integers in the array list:");
+	  System.out.println(arrayList);
+
+	  LinkedList linkedList = new LinkedList(arrayList);
+	  linkedList.add(1,"beautiful");
+	  linkedList.removeLast();
+	  linkedList.addFirst("world");
+
+	  System.out.println("Display the linked list forward:");
+	  ListIterator listIterator =linkedList.listIterator();
+	  while(listIterator.hasNext()) {
+		  System.out.print(listIterator.next() +" ");
+	  }
+	  System.out.println();
+
+	  System.out.println("Display the linked list backward:");
+	  listIterator =linkedList.listIterator(linkedList.size());
+	  while (listIterator.hasPrevious())
+	  {
+		  System.out.print(listIterator.previous() + " " );
+	  }
+  }
+}
